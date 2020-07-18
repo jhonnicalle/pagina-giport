fe490b01074cb9eeb900f689c92047b321effe3b
Jhonni Calle <jhonnicalle@gmail.com>
2020-07-18 11:47:12 -0400

    Updates

---
+++ precache-manifest.97595c9ed89cc0adc14575ca2e590fe0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25994dcd6066d97e5457541f49d710e",
+    "url": "/pagina-giport/index.html"
+  },
+  {
+    "revision": "df52c8e9df4464e36ade",
+    "url": "/pagina-giport/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6a4a34062cab8534dde9",
+    "url": "/pagina-giport/static/css/main.7b611ae1.chunk.css"
+  },
+  {
+    "revision": "df52c8e9df4464e36ade",
+    "url": "/pagina-giport/static/js/2.c2c6cfbf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pagina-giport/static/js/2.c2c6cfbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4a34062cab8534dde9",
+    "url": "/pagina-giport/static/js/main.d08840c0.chunk.js"
+  },
+  {
+    "revision": "7301a0f5acdb30ac1bff",
+    "url": "/pagina-giport/static/js/runtime-main.0f114f26.js"
+  },
+  {
+    "revision": "b3f1031dfda50f6601491389a591dc84",
+    "url": "/pagina-giport/static/media/giport-logo.b3f1031d.png"
+  },
+  {
+    "revision": "a9b550cfda888eb75f4d67854babd22a",
+    "url": "/pagina-giport/static/media/imagen-1.a9b550cf.jpg"
+  }
+]);
