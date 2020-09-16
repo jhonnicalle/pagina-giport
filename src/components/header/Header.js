37f403d57df297e86d0d8203d80dfdce2585267d
Jhonni Calle <jhonnicalle@gmail.com>
2020-09-16 08:33:28 -0400

    Adding left logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,8 +3,26 @@ import './header.css'
 // import styled from 'styled-components'
 // import logo from '../../images/giport-logo.png'
 import logo from '../../images/LOGO-GIPORT-03.png'
+import logo2 from '../../images/LOGO-GIPORT-04.png'
+import { Container, Grid, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    height: 'calc(100vh - 50px)',
+    // [theme.breakpoints.down('md')]: {
+    //   display: 'inline',
+    // },
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
   
     return(
       <div className="background-screen">
@@ -14,14 +32,27 @@ const Header = () => {
               <img src={logo} className="logo" alt={"Imagen de buques en el mar"} />
             </div>
           </div>
-          <div className="portada">
-            <div className="home-info">
-              <h1 className="title">BIENVENIDOS A GIPORT</h1>
-              <p>En Giport SAS ofrecemos un amplio portafolio de servicios orientado a satisfacer todas las necesidades del sector portuario y logístico.</p>
-            
-              <p>Brindamos un portafolio de servicios que proporcionan asistencia integral en todas las operaciones que demanden nuestros clientes, siempre con los estándares más altos.</p>
-            </div>
-          </div>
+          {/* <div className="portada"> */}
+          {/* <div className={classes.root}> */}
+          <Container maxWidth="lg">
+            <Grid container className={classes.root} direction="row" justify="center" alignItems="center">
+              <Grid item className={[classes.paper, "section_logo"]} xs={0} md={4} >
+                <div>
+                  <img src={logo2} className="logo2" alt={"Logo de empresa"}/>
+                </div>
+              </Grid>
+              <Grid item className={classes.paper} xs={12} md={8} >
+                <div className="home-info">
+                  <h1 className="title">BIENVENIDOS A GIPORT</h1>
+                  <p>En Giport SAS ofrecemos un amplio portafolio de servicios orientado a satisfacer todas las necesidades del sector portuario y logístico.</p>
+                
+                  <p>Brindamos un portafolio de servicios que proporcionan asistencia integral en todas las operaciones que demanden nuestros clientes, siempre con los estándares más altos.</p>
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
+          {/* </div>             */}
+          {/* </div> */}
         </div>
       </div>
     )
